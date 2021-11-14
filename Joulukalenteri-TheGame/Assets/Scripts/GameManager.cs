7e073f8ac4415feba23122d2903557ee2cb3d251
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-11-14 16:10:48 +0200

    Starting

---
+++ Joulukalenteri-TheGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void StartGameNow()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
